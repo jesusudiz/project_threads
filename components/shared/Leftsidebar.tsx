f786bca9a86a12f3f4d1296ccb9f2ea89da05e77
jesusudiz <jesusgabrieludiz@gmail.com>
2023-12-23 19:47:22 -0300

    create constants for use sidebar

--- components/shared/Leftsidebar.tsx
+++ components/shared/Leftsidebar.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Leftsidebar = () => {
   return (
-    <h1>
-      Leftsidebar
-    </h1>
+    <section className='custom-scrollbar leftsidebar'>
+
+      <div className="flex w-full flex-1 flex-col gap-6 px-6"></div>
+    </section>
   )
 }
 
